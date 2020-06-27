a02d4d773a6d3a7d77694a985dd02e0f49751c90
Ray Tsang <saturnism@gmail.com>
2020-06-26 22:23:15 -0400

    upgrade to spring boot 2.3.1, and containerize w/ buildpack

--- scripts/containerize.sh
+++ scripts/containerize.sh
@@ -15,10 +15,26 @@ cd ..
 mvn --also-make dependency:tree | grep maven-dependency-plugin | awk '{ print $(NF-1) }' | grep -v parent | while read module; do
   pushd $module
 
+  mvn -DskipTests package
+
+  echo "Build with Docker"
   docker build -t "${IMAGE_PREFIX}/${module}-docker" .
   docker push "${IMAGE_PREFIX}/${module}-docker"
 
-  mvn -DskipTests compile com.google.cloud.tools:jib-maven-plugin:2.4.0:build -Dimage="${IMAGE_PREFIX}/${module}-jib"
+  echo "Build with Buildpack"
+  mvn -DskipTests spring-boot:build-image \
+    -Dspring-boot.build-image.imageName="${IMAGE_PREFIX}/${module}-buildpack"
+  docker push "${IMAGE_PREFIX}/${module}-buildpack"
+
+  echo "Build with GCP Buildpack"
+  mvn -DskipTests spring-boot:build-image \
+    -Dspring-boot.build-image.builder=gcr.io/buildpacks/builder \
+    -Dspring-boot.build-image.imageName="${IMAGE_PREFIX}/${module}-buildpack-gcp"
+  docker push "${IMAGE_PREFIX}/${module}-buildpack-gcp"
+
+  echo "Build with Jib"
+  mvn -DskipTests compile com.google.cloud.tools:jib-maven-plugin:2.4.0:build \
+    -Dimage="${IMAGE_PREFIX}/${module}-jib"
 
   popd
 done
