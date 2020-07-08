2ce9d3a7ce885aae7bb8c211754cadb293d76d85
Ray Tsang <saturnism@gmail.com>
2020-07-08 12:06:09 -0400

    deploys everything

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,34 +4,33 @@ SCRIPT_DIR=$(dirname $(realpath "$0"))
 cd "$SCRIPT_DIR"
 
 function deploy_appengine {
-  project_id=$1
-  service_name=$2
-  instance_class=$3
-  jar=$4
-  java_tool_options=$5
+  local project_id=$1
+  local service_name=$2
+  local instance_class=$3
+  local jar=$4
+  local java_tool_options=$5
 
-  appyaml_file=$(mktemp /tmp/app.yaml.XXXXXX)
+  local appyaml_file=$(mktemp /tmp/app.yaml.XXXXXX)
 
   cat "$SCRIPT_DIR/app.yaml.tmpl" | \
   service_name="$service_name" instance_class="$instance_class" java_tool_options="$java_tool_options" \
-  envsubst > $appyaml_file
+    envsubst > $appyaml_file
 
   gcloud -q --project="$project_id" app deploy "$jar" --appyaml="$appyaml_file" -v 1
 
   rm $appyaml_file
-
 }
 
 function deploy_cloudrun {
-  project_id=$1
-  region=$2
-  service_name=$3
-  image=$4
-  cpus=$5
-  memory=$6
-  java_tool_options=$7
+  local project_id=$1
+  local region=$2
+  local service_name=$3
+  local image=$4
+  local cpus=$5
+  local memory=$6
+  local java_tool_options=$7
 
-  echo gcloud -q --project="$project_id" run deploy "$service_name" --region="$region" --image="$image" --set-env-vars="JAVA_TOOL_OPTIONS=${java_tool_options}" --platform managed --allow-unauthenticated
+  gcloud -q --project="$project_id" run deploy "$service_name" --region="$region" --image="$image" --set-env-vars="JAVA_TOOL_OPTIONS=${java_tool_options}" --platform managed --allow-unauthenticated
 }
 
 if [ "$1" != "" ]; then
@@ -52,107 +51,114 @@ mvn --also-make dependency:tree | grep maven-dependency-plugin | awk '{ print $(
 
   mvn -pl "$module" package -DskipTests
 
+  prefix="helloworld-"
+  service_name=${module#$prefix};
+
+  echo "SERVICE NAME: ${service_name}"
 
   echo "Deploy to App Engine, F1"
-  deploy_appengine "$PROJECT_ID" "${module}" "F1" "${module}/target/helloworld.jar" ""
+  deploy_appengine "$PROJECT_ID" "${service_name}" "F1" "${module}/target/helloworld.jar" ""
 
   echo "Deploy to App Engine, F2"
-  deploy_appengine "$PROJECT_ID" "${module}" "F2" "${module}/target/helloworld.jar" ""
+  deploy_appengine "$PROJECT_ID" "${service_name}-f2" "F2" "${module}/target/helloworld.jar" ""
 
   echo "Deploy to App Engine, F1, Tiered Compliation"
-  deploy_appengine "$PROJECT_ID" "${module}-tc" "F1" "${module}/target/helloworld.jar" "${TIERED_COMPILIATON_FLAGS}"
+  deploy_appengine "$PROJECT_ID" "${service_name}-tc" "F1" "${module}/target/helloworld.jar" "${TIERED_COMPILIATON_FLAGS}"
 
   echo "Deploy to App Engine, F2, Tiered Compliation"
-  deploy_appengine "$PROJECT_ID" "${module}-tc" "F2" "${module}/target/helloworld.jar" "${TIERED_COMPILIATON_FLAGS}"
+  deploy_appengine "$PROJECT_ID" "${service_name}-tc-f2" "F2" "${module}/target/helloworld.jar" "${TIERED_COMPILIATON_FLAGS}"
 
   echo "Deploy to App Engine, F1, Lazy Init"
-  deploy_appengine "$PROJECT_ID" "${module}-lazy" "F1" "${module}/target/helloworld.jar" "${LAZY_INIT_FLAGS}"
+  deploy_appengine "$PROJECT_ID" "${service_name}-lazy" "F1" "${module}/target/helloworld.jar" "${LAZY_INIT_FLAGS}"
 
   echo "Deploy to App Engine, F2, Lazy Init"
-  deploy_appengine "$PROJECT_ID" "${module}-lazy" "F2" "${module}/target/helloworld.jar" "${LAZY_INIT_FLAGS}"
+  deploy_appengine "$PROJECT_ID" "${service_name}-lazy-f2" "F2" "${module}/target/helloworld.jar" "${LAZY_INIT_FLAGS}"
 
   echo "Deploy to App Engine, F1, Lazy Init, Tiered Compliation"
-  deploy_appengine "$PROJECT_ID" "${module}-lazy-tc" "F1" "${module}/target/helloworld.jar" "${TIERED_COMPILIATON_FLAGS} ${LAZY_INIT_FLAGS}"
+  deploy_appengine "$PROJECT_ID" "${service_name}-lazy-tc" "F1" "${module}/target/helloworld.jar" "${TIERED_COMPILIATON_FLAGS} ${LAZY_INIT_FLAGS}"
 
   echo "Deploy to App Engine, F2, Lazy Init, Tiered Compliation"
-  deploy_appengine "$PROJECT_ID" "${module}-lazy-tc" "F2" "${module}/target/helloworld.jar" "${TIERED_COMPILIATON_FLAGS} ${LAZY_INIT_FLAGS}"
+  deploy_appengine "$PROJECT_ID" "${service_name}-lazy-tc-f2" "F2" "${module}/target/helloworld.jar" "${TIERED_COMPILIATON_FLAGS} ${LAZY_INIT_FLAGS}"
 
   image_name="${IMAGE_PREFIX}/${module}-jib"
 
   echo "Deploy Jib image to Cloud Run, 1CPU"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-jib" "${image_name}" "1" "256M" ""
+  deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-jib" "${image_name}" "1" "256M" ""
 
   echo "Deploy Jib image to Cloud Run, 2CPU"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-jib" "${image_name}" "2" "256M" ""
+  deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-jib-2cpu" "${image_name}" "2" "256M" ""
 
   echo "Deploy Jib image to Cloud Run, 1CPU, Tiered Compliation"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-jib-tc" "${image_name}" "1" "256M" "${TIERED_COMPILIATON_FLAGS}"
+  deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-jib-tc" "${image_name}" "1" "256M" "${TIERED_COMPILIATON_FLAGS}"
 
   echo "Deploy Jib image to Cloud Run, 2CPU, Tiered Compliation"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-jib-tc" "${image_name}" "2" "256M" "${TIERED_COMPILIATON_FLAGS}"
+  deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-jib-tc-2cpu" "${image_name}" "2" "256M" "${TIERED_COMPILIATON_FLAGS}"
 
   echo "Deploy Jib image to Cloud Run, 1CPU, Lazy Init"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-jib-lazy" "${image_name}" "1" "256M" "${LAZY_INIT_FLAGS}"
+  deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-jib-lazy" "${image_name}" "1" "256M" "${LAZY_INIT_FLAGS}"
 
   echo "Deploy Jib image to Cloud Run, 2CPU, Lazy Init"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-jib-lazy" "${image_name}" "2" "256M" "${LAZY_INIT_FLAGS}"
+  deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-jib-lazy-2cpu" "${image_name}" "2" "256M" "${LAZY_INIT_FLAGS}"
 
   echo "Deploy Jib image to Cloud Run, 1CPU, Lazy Init, Tiered Compliation"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-jib-lazy-tc" "${image_name}" "1" "256M" "${TIERED_COMPILIATON_FLAGS} ${LAZY_INIT_FLAGS}"
+  deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-jib-lazy-tc" "${image_name}" "1" "256M" "${TIERED_COMPILIATON_FLAGS} ${LAZY_INIT_FLAGS}"
 
   echo "Deploy Jib image to Cloud Run, 2CPU, Lazy Init, Tiered Compliation"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-jib-lazy-tc" "${image_name}" "2" "256M" "${TIERED_COMPILIATON_FLAGS} ${LAZY_INIT_FLAGS}"
+  deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-jib-lazy-tc-2cpu" "${image_name}" "2" "256M" "${TIERED_COMPILIATON_FLAGS} ${LAZY_INIT_FLAGS}"
 
 
   image_name="${IMAGE_PREFIX}/${module}-docker"
 
   echo "Deploy Docker image to Cloud Run, 1CPU"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-docker" "${image_name}" "1" "256M" ""
+  deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-docker" "${image_name}" "1" "256M" ""
 
   echo "Deploy Docker image to Cloud Run, 2CPU"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-docker" "${image_name}" "2" "256M" ""
+  deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-docker-2cpu" "${image_name}" "2" "256M" ""
 
   echo "Deploy Docker image to Cloud Run, 1CPU, Tiered Compliation"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-docker-tc" "${image_name}" "1" "256M" "${TIERED_COMPILIATON_FLAGS}"
+  deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-docker-tc" "${image_name}" "1" "256M" "${TIERED_COMPILIATON_FLAGS}"
 
   echo "Deploy Docker image to Cloud Run, 2CPU, Tiered Compliation"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-docker-tc" "${image_name}" "2" "256M" "${TIERED_COMPILIATON_FLAGS}"
+  deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-docker-tc-2cpu" "${image_name}" "2" "256M" "${TIERED_COMPILIATON_FLAGS}"
 
   echo "Deploy Docker image to Cloud Run, 1CPU, Lazy Init"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-docker-lazy" "${image_name}" "1" "256M" "${LAZY_INIT_FLAGS}"
+  deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-docker-lazy" "${image_name}" "1" "256M" "${LAZY_INIT_FLAGS}"
 
   echo "Deploy Docker image to Cloud Run, 2CPU, Lazy Init"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-docker-lazy" "${image_name}" "2" "256M" "${LAZY_INIT_FLAGS}"
+  deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-docker-lazy-2cpu" "${image_name}" "2" "256M" "${LAZY_INIT_FLAGS}"
 
   echo "Deploy Docker image to Cloud Run, 1CPU, Lazy Init, Tiered Compliation"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-docker-lazy-tc" "${image_name}" "1" "256M" "${TIERED_COMPILIATON_FLAGS} ${LAZY_INIT_FLAGS}"
+  deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-docker-lazy-tc" "${image_name}" "1" "256M" "${TIERED_COMPILIATON_FLAGS} ${LAZY_INIT_FLAGS}"
 
   echo "Deploy Docker image to Cloud Run, 2CPU, Lazy Init, Tiered Compliation"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-docker-lazy-tc" "${image_name}" "2" "256M" "${TIERED_COMPILIATON_FLAGS} ${LAZY_INIT_FLAGS}"
+  deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-docker-lazy-tc-2cpu" "${image_name}" "2" "256M" "${TIERED_COMPILIATON_FLAGS} ${LAZY_INIT_FLAGS}"
 
-  echo "Deploy Docker image to Cloud Run, 1CPU, AppCDS"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-docker-appcds" "${image_name}" "1" "256M" "${APPCDS_FLAGS}"
+  
+  if [ -f "$module/.appcds" ]; then
+    echo "Deploy Docker image to Cloud Run, 1CPU, AppCDS"
+    deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-docker-appcds" "${image_name}" "1" "256M" "${APPCDS_FLAGS}"
 
-  echo "Deploy Docker image to Cloud Run, 2CPU, AppCDS"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-docker-appcds" "${image_name}" "2" "256M" "${APPCDS_FLAGS}"
+    echo "Deploy Docker image to Cloud Run, 2CPU, AppCDS"
+    deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-docker-appcds-2cpu" "${image_name}" "2" "256M" "${APPCDS_FLAGS}"
 
-  echo "Deploy Docker image to Cloud Run, 1CPU, AppCDS, Lazy Init"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-docker-appcds-lazy" "${image_name}" "1" "256M" "${APPCDS_FLAGS} ${LAZY_INIT_FLAGS}"
+    echo "Deploy Docker image to Cloud Run, 1CPU, AppCDS, Lazy Init"
+    deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-docker-appcds-lazy" "${image_name}" "1" "256M" "${APPCDS_FLAGS} ${LAZY_INIT_FLAGS}"
 
-  echo "Deploy Docker image to Cloud Run, 2CPU, AppCDS, Lazy Init"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-docker-appcds-lazy" "${image_name}" "2" "256M" "${APPCDS_FLAGS} ${LAZY_INIT_FLAGS}"
+    echo "Deploy Docker image to Cloud Run, 2CPU, AppCDS, Lazy Init"
+    deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-docker-appcds-lazy-2cpu" "${image_name}" "2" "256M" "${APPCDS_FLAGS} ${LAZY_INIT_FLAGS}"
 
-  echo "Deploy Docker image to Cloud Run, 1CPU, AppCDS, Tiered Compliation"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-docker-appcds-tc" "${image_name}" "1" "256M" "${APPCDS_FLAGS} ${TIERED_COMPILIATON_FLAGS}"
+    echo "Deploy Docker image to Cloud Run, 1CPU, AppCDS, Tiered Compliation"
+    deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-docker-appcds-tc" "${image_name}" "1" "256M" "${APPCDS_FLAGS} ${TIERED_COMPILIATON_FLAGS}"
 
-  echo "Deploy Docker image to Cloud Run, 2CPU, AppCDS, Tiered Compliation"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-docker-appcds-tc" "${image_name}" "2" "256M" "${APPCDS_FLAGS} ${TIERED_COMPILIATON_FLAGS}"
+    echo "Deploy Docker image to Cloud Run, 2CPU, AppCDS, Tiered Compliation"
+    deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-docker-appcds-tc-2cpu" "${image_name}" "2" "256M" "${APPCDS_FLAGS} ${TIERED_COMPILIATON_FLAGS}"
 
-  echo "Deploy Docker image to Cloud Run, 1CPU, AppCDS, Lazy Init, Tiered Compliation"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-docker-appcds-lazy-tc" "${image_name}" "1" "256M" "${APPCDS_FLAGS} ${TIERED_COMPILIATON_FLAGS} ${LAZY_INIT_FLAGS}"
+    echo "Deploy Docker image to Cloud Run, 1CPU, AppCDS, Lazy Init, Tiered Compliation"
+    deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-docker-appcds-lazy-tc" "${image_name}" "1" "256M" "${APPCDS_FLAGS} ${TIERED_COMPILIATON_FLAGS} ${LAZY_INIT_FLAGS}"
 
-  echo "Deploy Docker image to Cloud Run, 2CPU, AppCDS, Lazy Init, Tiered Compliation"
-  deploy_cloudrun "$PROJECT_ID" "us-central1" "${module}-docker-appcds-lazy-tc" "${image_name}" "2" "256M" "${APPCDS_FLAGS} ${TIERED_COMPILIATON_FLAGS} ${LAZY_INIT_FLAGS}"
+    echo "Deploy Docker image to Cloud Run, 2CPU, AppCDS, Lazy Init, Tiered Compliation"
+    deploy_cloudrun "$PROJECT_ID" "us-central1" "${service_name}-docker-appcds-lazy-tc-2cpu" "${image_name}" "2" "256M" "${APPCDS_FLAGS} ${TIERED_COMPILIATON_FLAGS} ${LAZY_INIT_FLAGS}"
+  fi
 done
 
 
