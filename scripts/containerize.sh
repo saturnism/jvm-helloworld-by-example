a77094a072d6bb7e1dadb669af73982f7ed9ad0b
Ray Tsang <saturnism@gmail.com>
2020-07-22 16:24:38 -0400

    ability to containerize just one module

--- scripts/containerize.sh
+++ scripts/containerize.sh
@@ -1,45 +1,55 @@
 #!/bin/bash
 
-if [ "$1" != "" ]; then
-  PROJECT_ID=$1
-else
-  echo "$0 [project id]"
-  exit 1
-fi
-
-IMAGE_PREFIX="gcr.io/${PROJECT_ID}"
-
 cd "$(dirname "$0")"
 cd ..
 
-mvn --also-make dependency:tree | grep maven-dependency-plugin | awk '{ print $(NF-1) }' | grep -v parent | while read module; do
+if [ "$1" != "" ]; then
+  PROJECT_ID=$1
+else
+  echo "$0 projectId [module]"
+  exit 1
+fi
+
+function containerize {
+  local project_id=$1
+  local module=$2
+  local image_prefix="gcr.io/${PROJECT_ID}"
+
   pushd $module
 
   mvn -DskipTests package
 
   echo "Build with Docker"
-  docker build -t "${IMAGE_PREFIX}/${module}-docker" .
-  docker push "${IMAGE_PREFIX}/${module}-docker"
+  docker build -t "${image_prefix}/${module}-docker" .
+  docker push "${image_prefix}/${module}-docker"
 
   if [[ "$module" == *"springboot-"* ]]; then
     echo "Build with Spring Boot Buildpack"
     mvn -DskipTests spring-boot:build-image \
-      -Dspring-boot.build-image.imageName="${IMAGE_PREFIX}/${module}-buildpack"
-    docker push "${IMAGE_PREFIX}/${module}-buildpack"
+      -Dspring-boot.build-image.imageName="${image_prefix}/${module}-buildpack"
+    docker push "${image_prefix}/${module}-buildpack"
 
     echo "Build with GCP Buildpack"
     mvn -DskipTests spring-boot:build-image \
       -Dspring-boot.build-image.builder=gcr.io/buildpacks/builder \
-      -Dspring-boot.build-image.imageName="${IMAGE_PREFIX}/${module}-buildpack-gcp"
-    docker push "${IMAGE_PREFIX}/${module}-buildpack-gcp"
+      -Dspring-boot.build-image.imageName="${image_prefix}/${module}-buildpack-gcp"
+    docker push "${image_prefix}/${module}-buildpack-gcp"
   fi
 
   echo "Build with Jib"
   mvn -DskipTests compile com.google.cloud.tools:jib-maven-plugin:2.4.0:build \
-    -Dimage="${IMAGE_PREFIX}/${module}-jib"
+    -Dimage="${image_prefix}/${module}-jib"
 
   popd
-done
+}
 
-
+if [ "$2" != "" ]; then
+  echo "Containerize module $2"
+  containerize $PROJECT_ID $2
+else
+  echo "Containerize all modules..."
+  mvn --also-make dependency:tree | grep maven-dependency-plugin | awk '{ print $(NF-1) }' | grep -v parent | while read module; do
+    containerize $PROJECT_ID $module
+  done
+fi
 
