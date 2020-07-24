47bd753f143861c67d46b8d107ca85d2f42066c6
Ray Tsang <saturnism@gmail.com>
2020-07-24 13:54:53 -0400

    updated

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -5,16 +5,22 @@ cd "$SCRIPT_DIR/.."
 if [ "$1" != "" ]; then
   REPORT_DIR="$1"
 else
-  echo "$0 reports_directory"
+  echo "$0 reports_directory [pattern]"
   exit 1
 fi
 
-pushd $REPORT_DIR >&2
+PATTERN="*.txt"
+if [ "$2" != "" ]; then
+  PATTERN="$2"
+fi
 
-echo -e "name,deploy_time,loadtest_duration,latency_min,latency_max,latency_p90,latency_p95,latency_p99,success_rate,startup_min,startup_max,startup_p90,startup_p95,startup_p99"
-for f in *.txt; do
-  name=${f%.txt}
 
+function print_header {
+  echo -e "name,deploy_time,loadtest_duration,latency_min,latency_max,latency_p90,latency_p95,latency_p99,success_rate,startup_min,startup_max,startup_p90,startup_p95,startup_p99"
+}
+
+function report {
+  local name="$1"
   echo -e "${name}" >&2
 
   n=$(tail -n +2 $f | cut -d, -f1 | st --count)
@@ -25,25 +31,25 @@ for f in *.txt; do
   failures=$((n - successes))
   success_rate=$((successes / n * 100))
   failure_rate=$((failures / n * 100))
-  p90=$(tail -n +2 $f | cut -d, -f1 | st --percentile=90)
-  p95=$(tail -n +2 $f | cut -d, -f1 | st --percentile=95)
-  p99=$(tail -n +2 $f | cut -d, -f1 | st --percentile=99)
+  p90=$(tail -n +2 $f | cut -d, -f1 | st --percentile=90 --format="%.2f")
+  p95=$(tail -n +2 $f | cut -d, -f1 | st --percentile=95 --format="%.2f")
+  p99=$(tail -n +2 $f | cut -d, -f1 | st --percentile=99 --format="%.2f")
 
   deploy_time=$(grep real ${name}.time | sed -e 's/^real[[:space:]]*//')
 
   s_unit=$(head -n1 "${name}.log" | sed -e 's/.* in \(.*\)/\1/' | cut -d" " -f2)
-  s_min=$(cat "${name}.log" | sed -e 's/.* in \(.*\)/\1/' | cut -d" " -f1 | st --min)
-  s_max=$(cat "${name}.log" | sed -e 's/.* in \(.*\)/\1/' | cut -d" " -f1 | st --max)
-  s_p90=$(cat "${name}.log" | sed -e 's/.* in \(.*\)/\1/' | cut -d" " -f1 | st --percentile=90)
-  s_p95=$(cat "${name}.log" | sed -e 's/.* in \(.*\)/\1/' | cut -d" " -f1 | st --percentile=95)
-  s_p99=$(cat "${name}.log" | sed -e 's/.* in \(.*\)/\1/' | cut -d" " -f1 | st --percentile=99)
+  s_min=$(cat "${name}.log" | sed -e 's/.* in \(.*\)/\1/' | cut -d" " -f1 | st --min --format="%.2f")
+  s_max=$(cat "${name}.log" | sed -e 's/.* in \(.*\)/\1/' | cut -d" " -f1 | st --max --format="%.2f")
+  s_p90=$(cat "${name}.log" | sed -e 's/.* in \(.*\)/\1/' | cut -d" " -f1 | st --percentile=90 --format="%.2f")
+  s_p95=$(cat "${name}.log" | sed -e 's/.* in \(.*\)/\1/' | cut -d" " -f1 | st --percentile=95 --format="%.2f")
+  s_p99=$(cat "${name}.log" | sed -e 's/.* in \(.*\)/\1/' | cut -d" " -f1 | st --percentile=99 --format="%.2f")
 
   if [ "${s_unit}" == "ms" ]; then
-    s_min=$(bc <<< "scale=3; ${s_min}/1000 ")
-    s_max=$(bc <<< "scale=3; ${s_max}/1000 ")
-    s_p90=$(bc <<< "scale=3; ${s_p90}/1000 ")
-    s_p95=$(bc <<< "scale=3; ${s_p95}/1000 ")
-    s_p99=$(bc <<< "scale=3; ${s_p99}/1000 ")
+    s_min=$(bc <<< "scale=2; ${s_min}/1000 ")
+    s_max=$(bc <<< "scale=2; ${s_max}/1000 ")
+    s_p90=$(bc <<< "scale=2; ${s_p90}/1000 ")
+    s_p95=$(bc <<< "scale=2; ${s_p95}/1000 ")
+    s_p99=$(bc <<< "scale=2; ${s_p99}/1000 ")
   fi
 
   echo -e "${name},${deploy_time},${duration},${min},${max},${p90},${p95},${p99},$(printf %.2f ${success_rate})%,${s_min},${s_max},${s_p90},${s_p95},${s_p99}"
@@ -52,7 +58,13 @@ for f in *.txt; do
   echo -e "- Latency: min=${min} max=${max} p90=${p90} p95=${p95} p99=${p99}" >&2
   echo -e "- Success Rate: success=$(printf %.2f ${success_rate})% failure=$(printf %.2f ${failure_rate})%" >&2
   echo -e "- Durations: deploy_time=${deploy_time} loadtest_duration=${duration}" >&2
+}
 
+pushd $REPORT_DIR >&2
+
+print_header
+for f in $PATTERN; do
+  name=${f%.txt}
+  report "$name"
 done
-
 popd >&2
